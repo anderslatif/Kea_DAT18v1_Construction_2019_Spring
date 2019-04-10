30f34a024216a809cc40b41fbb4d800e6c53c701
anders <al@mobilize-systems.com>
2019-04-10 14:56:49 +0200

    Updated my info and catcontroller

--- 5.Spring_Recap_Cats/src/main/java/edu/kea/cats/controller/CatController.java
+++ 5.Spring_Recap_Cats/src/main/java/edu/kea/cats/controller/CatController.java
@@ -1,6 +1,7 @@
 package edu.kea.cats.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +9,21 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class CatController {
 
-    @GetMapping(name = "/profile")
-    public String catGreeting() {
+    @GetMapping(name = "/")
+    public String showHomePage() {
         return "index.html";
     }
+
+    @GetMapping("/profile")
+    public String showProfile(Model model) {
+        return "profile/profile-page.html";
+    }
+
+    @GetMapping("/test")
+    public @ResponseBody String tryTest() {
+        return "test works... nice one";
+    }
+
 
 }
 
