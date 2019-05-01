0e6e04c18932a01206aecaecd4c45cd9330427c5
anders <al@mobilize-systems.com>
2019-05-01 11:25:34 +0200

    Added the call to the cats API

--- 6.Cats_Thymeleaf/src/main/java/edu/kea/cats/controller/CatController.java
+++ 6.Cats_Thymeleaf/src/main/java/edu/kea/cats/controller/CatController.java
@@ -6,12 +6,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class CatController {
 
     @GetMapping(name = "/")
     public String showHomePage() {
+        getURL();
         return "index.html";
     }
 
@@ -20,6 +22,14 @@ public class CatController {
         Cat modelCat = new Cat(2, "Karl Marx", "");
         model.addAttribute("cat", modelCat);
         return "profile/profile.html";
+    }
+
+    public String getURL() {
+        String url = "https://aws.random.cat/meow";
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(url, String.class);
+        System.out.println(result);
+        return result;
     }
 
 }
