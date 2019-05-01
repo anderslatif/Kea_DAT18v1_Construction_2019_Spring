8be17c670e090ac001b51b7ef727ec6f832bbd13
anders <al@mobilize-systems.com>
2019-05-01 12:56:29 +0200

    Added the JSON parsing

--- 6.Cats_Thymeleaf/src/main/java/edu/kea/cats/controller/CatController.java
+++ 6.Cats_Thymeleaf/src/main/java/edu/kea/cats/controller/CatController.java
@@ -1,6 +1,9 @@
 package edu.kea.cats.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
 import edu.kea.cats.model.Cat;
+import edu.kea.cats.model.CatImageResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,12 +11,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
+
 @Controller
 public class CatController {
 
     @GetMapping(name = "/")
     public String showHomePage() {
-        getURL();
+        System.out.println(getURL());
         return "index.html";
     }
 
@@ -28,8 +33,11 @@ public class CatController {
         String url = "https://aws.random.cat/meow";
         RestTemplate restTemplate = new RestTemplate();
         String result = restTemplate.getForObject(url, String.class);
-        System.out.println(result);
-        return result;
+        Gson gson = new Gson();
+        CatImageResponse catImageResponse = gson.fromJson(result, CatImageResponse.class);
+        return catImageResponse.getFile();
     }
+
+
 
 }
---
+++ 6.Cats_Thymeleaf/src/main/java/edu/kea/cats/model/CatImageResponse.java
@@ -0,0 +1,19 @@
+package edu.kea.cats.model;
+
+public class CatImageResponse {
+
+    private String file;
+
+    public CatImageResponse(String file) {
+        this.file = file;
+    }
+
+    public String getFile() {
+        return file;
+    }
+
+    public void setFile(String file) {
+        this.file = file;
+    }
+
+}
